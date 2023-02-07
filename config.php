b0d0efd0cc23d85b11da9be0c8b1a4d6dfac5bb8
Angelina Vuković <av20190218@student.fon.bg.ac.rs>
2023-02-07 22:25:58 +0100

    kreirana konfiguracija za bazu

--- config.php
+++ config.php
@@ -6,7 +6,7 @@
         $password = "";
         $database = "knjige";
         
+        
         $conn = mysqli_connect($hostname,$username,$password,$database) or die("Database connection failed");
-     
 
 ?>
